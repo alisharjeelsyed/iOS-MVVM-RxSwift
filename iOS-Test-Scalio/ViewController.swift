6fa6391260a7439942fdd519a81176bd94956d07
Sharjeel <Sharjeel.ali@lucidlane.com>
2021-12-10 08:16:10 +0500

    Initial Commit

---
+++ iOS-Test-Scalio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Test-Scalio
+//
+//  Created by Sharjeel Ali on 10/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
